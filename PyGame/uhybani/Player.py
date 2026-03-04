c1b199d66a55716a5c7f556fcf3d7e3210d8c5c0
Bighead126 <dmircevsky@gmail.com>
2026-03-04 09:27:47 +0100

    hrahra

--- PyGame/uhybani/Player.py
+++ PyGame/uhybani/Player.py
@@ -3,15 +3,17 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
-        self.image = pygame.image.load("obrazky/rocket.png")
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect(center = (x,y))
+        self.image = pygame.image.load(PLAYER_IMAGE_PATH).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PLAYER_WIDTH, PLAYER_HEIGHT))
+        self.rect = self.image.get_rect(bottom=y, centerx=x) # center = (x,y)¨
         self.speed = PLAYER_SPEED
+
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.speed
+            self.rect.x -= self.speed   
         if keys[pygame.K_RIGHT] and self.rect.right < WIDTH:
             self.rect.x += self.speed
 
-        
+if __name__ == "__main__":
+    import main
--- PyGame/uhybani/block.py
+++ PyGame/uhybani/block.py
@@ -1,11 +1,21 @@
 import pygame
+import random
 from settings import *
+
+x = random.randint(0, WIDTH - BLOCK_WIDTH)
+print(x)
+
 class Block(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
-        self.image = pygame.image.load("obrazky/diamant.png")
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect(center = (x,y))
+        self.image = pygame.image.load(BLOCK_IMAGE_PATH).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (BLOCK_WIDTH, BLOCK_HEIGHT))
+        self.rect = self.image.get_rect(bottom=y, centerx=x) # center = (x,y)¨
         self.speed = BLOCK_SPEED
-    def update(self):
         
+    def update(self):
+        self.rect.x = x
+        self.rect.y += self.speed
+        if self.rect.top > HEIGHT:
+            self.rect.bottom = 0
+            self.kill()
