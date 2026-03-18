26d8845f54d05f3b6768294c72e9d4e32993355f
Bighead126 <dmircevsky@gmail.com>
2026-03-18 09:31:02 +0100

    hh

--- PyGame/uhybani/block.py
+++ PyGame/uhybani/block.py
@@ -2,20 +2,18 @@ import pygame
 import random
 from settings import *
 
-x = random.randint(0, WIDTH - BLOCK_WIDTH)
-print(x)
-
 class Block(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self):
         super().__init__()
+        x = random.randrange(0 + (BLOCK_WIDTH //2), WIDTH - (BLOCK_WIDTH // 2))
+        y = 0
         self.image = pygame.image.load(BLOCK_IMAGE_PATH).convert_alpha()
         self.image = pygame.transform.scale(self.image, (BLOCK_WIDTH, BLOCK_HEIGHT))
         self.rect = self.image.get_rect(bottom=y, centerx=x) # center = (x,y)¨
         self.speed = BLOCK_SPEED
         
     def update(self):
-        self.rect.x = x
         self.rect.y += self.speed
-        if self.rect.top > HEIGHT:
-            self.rect.bottom = 0
+        if self.rect.top > HEIGHT+100:
             self.kill()
+            
--- PyGame/uhybani/main.py
+++ PyGame/uhybani/main.py
@@ -8,29 +8,47 @@ pygame.display.set_caption("Uhybani")
 running = True
 clock = pygame.time.Clock()
 
+score = 0
+
+text = "Score: {}".format(score)
+base_font = pygame.font.Font(None, 40)
+
 hrac = Player(WIDTH // 2, HEIGHT)
 hrac_group = pygame.sprite.Group()
 hrac_group.add(hrac)
-blok = Block(WIDTH // 2, 0)
+blok = Block()
 blok_group = pygame.sprite.Group()
 blok_group.add(blok)
 
+BLOK_SPAWN = pygame.USEREVENT + 1
+pygame.time.set_timer(BLOK_SPAWN, 500)
 
 while running:
     screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == BLOK_SPAWN:
+            blok_group.add(Block())
+            score += 1
+            text = "Score: {}".format(score)
+            score_text = base_font.render(text, True, (255, 255, 255))
     hrac_group.update()
     hrac_group.draw(screen)
     blok_group.update()
     blok_group.draw(screen)
+    
 
     if pygame.sprite.spritecollide(hrac, blok_group, True, pygame.sprite.collide_mask):
         print("KOLIZE!")
         pygame.time.delay(1000)
-
+        with open("score.txt", "w") as soubor:
+            soubor.write(str(score))
         running = False
+
+    score_text = base_font.render(text, True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
 
     pygame.display.update()
     clock.tick(FPS)
